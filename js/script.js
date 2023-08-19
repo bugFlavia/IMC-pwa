6e2237d593444a18623a6907461a8949449d5873
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2023-08-19 15:36:30 -0400

    conta

--- js/script.js
+++ js/script.js
@@ -12,8 +12,17 @@ function calcular(){
     var conta = peso/(altura*altura)
     document.getElementById("resultado").innerHTML = "Seu IMC é:"+ conta.toFixed(2);
 
-    if (conta<18){
-        document.getElementById("conclusão").innerHTML = "Abaixo do peso";
-        document.getElementById("conclusão").style.color = "Red";
+    if (conta<18.5){
+        document.getElementById("conclusao").innerHTML = "Abaixo do peso";
+    }else if (conta>= 18.5 && conta< 24.9){
+        document.getElementById("conclusao").innerHTML = "Peso normal";
+    }else if (conta > 25 && conta< 29.9 ){
+        document.getElementById("conclusao").innerHTML = "Excesso de peso";
+    }else if (conta > 30 && conta< 34.9 ){
+        document.getElementById("conclusao").innerHTML = "Obesidade classe I";
+    }else if (conta > 35 && conta< 39.9 ){
+        document.getElementById("conclusao").innerHTML = "Obesidade classe II";
+    }else if (conta >= 40){
+        document.getElementById("conclusao").innerHTML = "Obesidade classe III";
     }
 }
